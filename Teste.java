3687bd42426272f4cd11e367734cd0daeb6a70c5
Emilson Cardoso <emilsoncardoso@gmail.com>
2023-04-10 16:36:52 -0300

    Alterações no arquivo Teste.java

--- Teste.java
+++ Teste.java
@@ -2,6 +2,7 @@ package projeto;
 
 public class Teste {
   public static void main(String[] args) {
-    System.out.println("Imprimindo na tela!");
+    System.out.println("Imprimindo informações na tela!");
+    System.out.println("Validando o aprendizado");
   }
 }
